d760ea63ff66fd652999fb4491a1e0f692969f11
rabiul25 <rabiul25@gmail.com>
2020-09-04 22:00:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# razorpayintegration-4m5ubj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/razorpayintegration-4m5ubj)
---
+++ src/app/app.component.ts
@@ -0,0 +1,70 @@
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { ExternalLibraryService } from './util';
+
+declare let Razorpay: any;
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  constructor(private razorpayService: ExternalLibraryService, private cd:  ChangeDetectorRef) { }
+  name = 'Angular';
+  response;
+  razorpayResponse;
+  showModal = false;
+
+  ngOnInit() {
+    this.razorpayService
+      .lazyLoadLibrary('https://checkout.razorpay.com/v1/checkout.js')
+      .subscribe();
+  }
+
+  RAZORPAY_OPTIONS = {
+    "key": "rzp_test_WMHWnR6EYJOqTX",
+    "amount": "",
+    "name": "Novopay",
+    "order_id": "",
+    "description": "Load Wallet",
+    "image": "https://livestatic.novopay.in/resources/img/nodeapp/img/Logo_NP.jpg",
+    "prefill": {
+      "name": "",
+      "email": "test@test.com",
+      "contact": "",
+      "method": ""
+    },
+    "modal": {},
+    "theme": {
+      "color": "#0096C5"
+    }
+  };
+
+  public proceed() {
+    this.RAZORPAY_OPTIONS.amount = 100 + '00';
+
+    // binding this object to both success and dismiss handler
+    this.RAZORPAY_OPTIONS['handler'] = this.razorPaySuccessHandler.bind(this);
+
+    // this.showPopup();
+
+    let razorpay = new Razorpay(this.RAZORPAY_OPTIONS)
+    razorpay.open();
+  }
+
+  public razorPaySuccessHandler(response) {
+    console.log(response);
+    this.razorpayResponse = `Razorpay Response`;
+    this.showModal = true;
+    this.cd.detectChanges()
+    document.getElementById('razorpay-response').style.display = 'block';
+  }
+
+  public test() {
+    document.getElementById('response-modal').style.display = 'block';
+    this.response = `dummy text`;
+  }
+
+
+}
